903b1b55aaf089be6670237b5fec7fceec2280dd
jinolu <66093882+jinolu@users.noreply.github.com>
2021-07-31 21:51:29 -0700

    Delete hdp_t5.bash

--- Code/3. hdp-faster/hdp_t5.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for ((year=2016; year<=2018;year+=1)); do
-  ./hdp --verbose --train_data /Users/LuJinhong\ 1\ 2/Desktop/patent_method/hdp_data/preprocessed_vector/$year.dat --directory /Users/LuJinhong\ 1\ 2/Desktop/patent_method/hdp_data/HDP_results/$year
-done
